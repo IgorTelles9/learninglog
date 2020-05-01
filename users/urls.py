bb1ed4965c8e220f96806f413652b9f352289ce7
igortelles <igortelles09@gmail.com>
2020-05-01 18:33:26 -0300

    Downgrade para o Django==2.0 por questões de segurança e alterações simples nos templates

--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,19 @@
 """ Define padrões de URL para usuários """
 
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from .  import views
+from django.contrib.auth.views import login
+
+from . import views
+
 app_name = 'users'
 urlpatterns = [
-    # Login page
-    path('login/',
-    auth_views.LoginView.as_view(template_name='users/login.html'),
-    name='login'),
+    # Login page.
+    path('login/', login, {'template_name': 'users/login.html'},
+        name='login'),
 
-    # Logout page
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+     # Logout page.
+    path('logout/', views.logout_view, name='logout'),
 
-    # Register page
+   # Registration page.
     path('register/', views.register, name='register'),
 ]
